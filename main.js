f8f3dc785ecc41f6fa1b118527ea14421046376e
Heather Storey <heathernstorey@gmail.com>
2023-03-14 22:27:56 -0400

    add html display of api data

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// Get:
-//thumbnail, title, description, date_display, artist_display, place_of_origin (manifest.json)
-let showArt;
-
 
 // Function to get painting info when image is clicked
 async function clickedEvent(img_index) {
@@ -9,32 +5,20 @@ async function clickedEvent(img_index) {
   let artwork_name = document.getElementsByTagName('img')[img_index].attributes[2].value;
   let artist = document.getElementsByTagName('img')[img_index].attributes[3].value;
   
-//   let headers = new Headers([
-//     ['Content-Type', 'application/json']
-//     ['Accept', 'application/json']
-//     ['AIC-User-Agent', 'codingtempleproject/heathernstorey@gmail.com']
-//   ]);
-
   let request = new Request(`https://api.artic.edu/api/v1/artworks/${artwork_id}`, {
     method: 'GET',
-    // headers: headers
   });
 
   let result = await fetch(request)
 
   let response = await result.json();
 
-  console.log(response);
-  alert(`This is ${artwork_name} by ${artist}`)
-
-  if (showArt) {
-    artInfo();
-  }
-}
-
-function artInfo(url) {
-    let showArt = new alert(url);
-    return showArt.alert
+  console.log(response)
+  console.log(response.data.title);
+  console.log(response.data.artist_display)
+  info_js = document.getElementById("info")
+  info_js.innerHTML = (`The painting you have chosen is: <br> <i>${response.data.title}</i> <br> by ${response.data.artist_display}`)
+  my_img = document.getElementById(`${artwork_id}`)
 }
 
 function getArt(id, event) {
@@ -101,4 +85,8 @@ function getArt(id, event) {
             break;
         }
     }
+}
+
+function clearScreen() {
+    window.location.reload()
 }
